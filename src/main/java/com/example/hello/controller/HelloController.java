b8956b4af4b56190ee58884459609bf1b1ebdb73
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-26 15:45:16 +0900

    GET 구현

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/get-api")
 public class HelloController {
-
-    @RequestMapping("/hello")
-    public String hello(){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
     }
 
 }

966a7ab10138789c13ef88a87440dd80c85c7ed2
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-27 13:23:54 +0900

    Request2, 3

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
@@ -28,6 +31,21 @@ public class HelloController {
     @GetMapping(value = "/request")
     public String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
         return name + " " + email + " " + organization;
+    }
+
+    // Map 활용
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        param.entrySet().forEach(map->{
+            System.out.printf("key:%s value:%s\n", map.getKey(), map.getValue());
+        }); // (map ->{} 여기부터 하나의 메서드)
+        return "Request2 호출완료 되었습니다.";
+    }
+
+    // MemberDto 객체 생성 후  사용
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(MemberDto memberDto) {
+        return memberDto.toString();
     }
 
 }

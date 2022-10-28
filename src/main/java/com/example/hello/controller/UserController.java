271b02031b11e08bc48c5884c8b59abecd73e720
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-28 14:39:32 +0900

    UserDAO add, deleteAll 추가 /  UserController deleteAll

--- src/main/java/com/example/hello/controller/UserController.java
+++ src/main/java/com/example/hello/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dao.UserDao;
+import com.example.hello.domain.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
-
+    private final UserDao userDao;
+    @RequestMapping(value = "/delete")
+    public int deleteAll() {
+        return userDao.deleteAll();
+    }
 }
--- src/main/java/com/example/hello/dao/UserDao.java
+++ src/main/java/com/example/hello/dao/UserDao.java
@@ -1,17 +1,27 @@
 package com.example.hello.dao;
 
+import com.example.hello.domain.dto.UserRequestDto;
+import com.example.hello.domain.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
 @Component
 public class UserDao {
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public void add(UserRequestDto userRequestDto){
+        jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?);",
+                userRequestDto.getId(), userRequestDto.getName(), userRequestDto.getPassword());
     }
 
-    public int deleteAll() {
-        return this.jdbcTemplate.update("DELETE FROM users");
+    public int deleteAll(){
+        return jdbcTemplate.update("DELETE FROM users");
     }
+
 }

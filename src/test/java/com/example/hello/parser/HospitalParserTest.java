625f129e906b89a38443570c687fdeef539c7f19
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-01 14:20:48 +0900

    SQL DB연동 및 add() Test

--- src/test/java/com/example/hello/parser/HospitalParserTest.java
+++ src/test/java/com/example/hello/parser/HospitalParserTest.java
@@ -1,5 +1,6 @@
 package com.example.hello.parser;
 
+import com.example.hello.dao.HospitalDao;
 import com.example.hello.domain.dto.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,18 @@ class HospitalParserTest {
 
     @Autowired
     ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Autowired
+    HospitalDao hospitalDao;
+
+    @Test
+    @DisplayName("Hostpital이 insert가 잘 되는지")
+    void add()  {
+        HospitalParser hp = new HospitalParser();
+        Hospital hospital = hp.parse(line1);
+        hospitalDao.add(hospital);
+        // get이 없어서 assert는 눈으로
+    }
 
     @Test
     void name() throws IOException {

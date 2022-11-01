f0f61970f576b16476b3dce250deb7bbbdd46113
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-01 13:28:04 +0900

    Insert Query에 Value Mapping

---
+++ src/main/java/com/example/hello/dao/HospitalDao.java
@@ -0,0 +1,42 @@
+package com.example.hello.dao;
+
+
+import com.example.hello.domain.dto.Hospital;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HospitalDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // List<Hospital> -- 11만건 hospital
+    public void add(Hospital hospital) {
+        String sql = "INSERT INTO `sys`.`nation_wide_hospitals` " +
+                "(`id`, `open_service_name`, `open_local_government_code`, `management_number`, " +
+                "`license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, " +
+                "`road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, " +
+                "`patient_room_count`, `total_number_of_beds`, `total_area_size`) " +
+                "VALUES (?, ?, ?, ?, " +
+                "?, ?, ?, " +
+                "?, ?, ?, " +
+                "?, ?, ?, " +
+                "?, ?, ?);";
+        this.jdbcTemplate.update(sql,
+                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(), hospital.getManagementNumber(),
+                hospital.getLicenseDate(), hospital.getBusinessStatus(), hospital.getBusinessStatusCode(),
+                hospital.getPhone(), hospital.getFullAddress(), hospital.getRoadNameAddress(),
+                hospital.getHospitalName(), hospital.getBusinessTypeName(), hospital.getHealthcareProviderCount(),
+                hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize());
+    }
+}
+
+
+
+
+
+
+
